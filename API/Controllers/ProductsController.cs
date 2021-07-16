3c086a6a23cea09d1cb43382260cdd2512b204cd
Dmitriy Sokolov <sda.falcon@gmail.com>
2021-07-16 18:27:41 +0500

    -

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "single product";
+        }
+
+    }
+}
