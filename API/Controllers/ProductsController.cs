f2537c4847a08b7a662e89509b31016929d5a8aa
Dmitriy Sokolov <sda.falcon@gmail.com>
2021-07-19 18:30:41 +0500

    brands и types в репозитории

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,9 +2,7 @@
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -32,6 +30,22 @@ namespace API.Controllers
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             return await _productRepository.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            var brands = await _productRepository.GetProductBrandsAsync();
+
+            return Ok(brands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            var types = await _productRepository.GetProductTypesAsync();
+
+            return Ok(types);
         }
 
     }
--- Infrastructure/Data/Repositories/ProductRepository.cs
+++ Infrastructure/Data/Repositories/ProductRepository.cs
@@ -23,5 +23,15 @@ namespace Infrastructure.Data.Repositories
         {
             return await _storeContext.Products.ToListAsync();
         }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await _storeContext.ProductBrands.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _storeContext.ProductTypes.ToListAsync();
+        }
     }
 }
