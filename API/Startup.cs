eefd49b6f7118c3956ee2b7644a9b93c4d6f6e19
Dmitriy Sokolov <sda.falcon@gmail.com>
2021-07-20 12:35:35 +0500

    убрал лишнее

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,7 +24,6 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
             services.AddControllers();
